5c366c0167f58ccd44f1612d2121604ed14ae116
AltaVista <altavista444@yandex.ru>
2022-07-05 15:28:30 +0300

    add resourses

---
+++ pp1.1.3/src/main/resources/create_table_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Users` (
+                         `id` BIGINT NOT NULL AUTO_INCREMENT,
+                         `name` VARCHAR(15) NOT NULL,
+                         `last_name` VARCHAR(15) NOT NULL,
+                         `age` TINYINT NOT NULL,
+                         PRIMARY KEY (`id`)
+);
